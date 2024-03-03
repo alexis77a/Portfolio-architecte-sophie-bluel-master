7e74f794142c1d95b4f72ee445dcf035e92542e2
Alexis Antunez <ordinateur.travai1@gmail.com>
2024-03-03 08:02:18 +0100

    Reprise a zero / Filtre fonctionnelle

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -2,157 +2,156 @@ const allWorks = new Set();
 const allCats = new Set();
 const gallery = document.querySelector('.gallery');
 
-async function init(){
-    const works = await fetchdata("works")
-    for (const work of works) {
-        allWorks.add(work)
-    }
-    displayWorks()
-    displayFilter()
-    /*if (isAdmin) {
-        
-    } else {
-
-    }*/
-    //fetchWorks();
-    //fetchCategories();
-}
-init()
-
-/*function isAdmin(){
-
-}*/
-
-async function fetchdata(type){
-    const response = await fetch(`http://localhost:5678/api/${type}`)
-    if (!response.ok) {
-        throw new Error('Erreur du réseau');
-    } else {
-        return response.json()
-    }
-}
-
-function displayWorks(filter = 0){
-    let filteredWorks = allWorks
-    if (filter != 0) {
-        filteredWorks = [...allWorks].filter()
-    }
-    //create fragment + forof filtered + create figure/inner + gallery append fragment
-}
-
-
-function displayFilter(){
-    //code
-    setFilterListener()
-}
-
-function setFilterListener(){}
-
-// fetch fait une requête GET à l'URL pour récupérer les travaux
-async function fetchWorks() {
+async function init() {
     try {
-        const response = await fetch('http://localhost:5678/api/works');
+        const works = await fetchData("works");
+        const categories = await fetchData('categories');
         
-        if (!response.ok) {
-            throw new Error('Erreur du réseau');
+        for (const work of works) {
+            allWorks.add(work);
         }
+
+        for (const category of categories) {
+            allCats.add(category);
+        }
+
+        console.log("Données de 'Works'", allWorks);
+        console.log("Données de 'Categories'", allCats);
+
+        displayWorks();
+        displayFilter();
         
-        allWorks = await response.json();
-
-
-        console.log('Données des travaux récupérées', allWorks);
-
-        filteredWorks(allWorks);
-        
-        console.log('Travaux récupérés', allWorks);
+        // Vous pouvez appeler filteredWorks ici si nécessaire
     } catch (error) {
-        console.error('Erreur lors de la récupération des travaux', error);
+        console.error('Erreur lors de l\'initialisation:', error);
     }
 }
 
+// récupere les données "works" et "categories"
+async function fetchData(dataType) {
+    try {
+        const response = await fetch(`http://localhost:5678/api/${dataType}`);
+        if (!response.ok) {
+            throw new Error('Erreur du serveur');
+        }
+        const data = await response.json();
+        if (dataType === 'works') {
+            // ajoute les données à "allWorks"
+            for (const work of data) {
+                allWorks.add(work);
+            }
+            return data;
+        } else if (dataType === 'categories') {
+            // ajoute les catégories à "allCats"
+            for (const category of data) {
+                allCats.add(category);
+            }
+            return data;
+        }
+    } catch (error) {
+        console.error('Erreur de récupération des données:', error);
+        return [];
+    }
+}
 
-// Fonction pour afficher les travaux filtré sur la page HTML
-function filteredWorks(works) {
-    
+function displayWorks() {
+    const gallery = document.querySelector('.gallery');
+    const fragment = document.createDocumentFragment();
+    // cherche "work" dans "allWorks"
+    for (const work of allWorks) {
+        const figure = document.createElement('div');
+        figure.classList.add('figure');
+
+        const image = document.createElement('img');
+        image.src = work.imageUrl;
+        image.alt = work.title;
+
+        const caption = document.createElement('figcaption');
+        caption.textContent = work.title;
+
+        figure.appendChild(image);
+        figure.appendChild(caption);
+        fragment.appendChild(figure);
+    }
+
+    gallery.appendChild(fragment);
+    console.log("Travaux affichés")
+}
+function displayFilter() {
+    const filtersContainer = document.querySelector('#filters-container');
+    const fragment = document.createDocumentFragment();
+
+    // création de la catégorie "Tous"
+    const allCategory = document.createElement('div');
+    allCategory.classList.add('category');
+    allCategory.textContent = 'Tous';
+    fragment.appendChild(allCategory);
+
+    allCategory.addEventListener('click', () => {
+        console.log("Catégorie : all"); 
+        const filtered = filteredWorks('all');
+        displayFilteredWorks(filtered);
+    });
+    // cherche les "catégory" dans "allCats"
+        for (const category of allCats) {
+        // créer un élément div pour chaque catégorie
+        const categoryDiv = document.createElement('div');
+        categoryDiv.classList.add('category');
+        categoryDiv.textContent = category.name; // récupere le nom des catégories
+        fragment.appendChild(categoryDiv);
+
+        categoryDiv.addEventListener('click', () => {
+            const categoryId = category.id;
+            console.log("Catégorie :", categoryId);
+            const filtered = filteredWorks(categoryId);
+            
+            displayFilteredWorks(filtered);
+        });
+    }
+
+    filtersContainer.appendChild(fragment);
+    console.log("Filtre affiché")
+}
+function displayFilteredWorks(filteredWorks) {
+    const gallery = document.querySelector('.gallery');
     gallery.innerHTML = '';
 
-    const fragment = document.createDocumentFragment()
-    works.forEach(work => {
-        const figure = document.createElement('figure');
-        figure.innerHTML = `<img src="${work.imgUrl}" alt="${work.title}">
-        <figcaption>${work.title}</figcaption>`
+    const fragment = document.createDocumentFragment();
+    for (const work of filteredWorks) {
+        const figure = document.createElement('div');
+        figure.classList.add('figure');
 
+        const image = document.createElement('img');
+        image.src = work.imageUrl;
+        image.alt = work.title;
+
+        const caption = document.createElement('figcaption');
+        caption.textContent = work.title;
+
+        figure.appendChild(image);
+        figure.appendChild(caption);
         fragment.appendChild(figure);
-    });
-    gallery.appendChild(fragment)
+    }
+
+    gallery.appendChild(fragment);
 }
-
-
-// Fonction pour filtrer les travaux en fonction de l'ID de la catégorie
-function filterWorks(categoryId) {
-    let filteredResults = [];
+function filteredWorks(categoryId) {
+    const filteredWorks = [];
 
     if (categoryId === "all") {
-        filteredResults = allWorks; 
-    } else {
-        filteredResults = allWorks.filter(work => work.categoryId === categoryId); // Filtrer les travaux par catégorie
+        return Array.from(allWorks);
     }
 
-    filteredWorks(filteredResults);
-}
-
-
-
-// fonction async/recuperation des travaux
-async function FetchFilterWorks(categoryId) {
-    try {
-        const response = await fetch(`http://localhost:5678/api/works?categoryId=${categoryId}`);
-        
-        if (!response.ok) {
-            throw new Error('Erreur du réseau');
+    for (const work of allWorks) {
+        if (work.categoryId === categoryId) {
+            filteredWorks.push(work);
         }
-        
-        const works = await response.json();
-        
-        console.log('Travaux filtrés', works);
-    } catch (error) {
-        console.error('Erreur de récupération des travaux filtrés', error);
     }
-}
 
-
-// fetch fait une requête GET à l'URL pour récupérer les catégories
-async function fetchCategories() {
-    try {
-        const response = await fetch('http://localhost:5678/api/categories');
-        
-        if (!response.ok) {
-            throw new Error('Erreur du réseau');
-        }
-        
-        const categories = await response.json();
-        
-        const filtersContainer = document.getElementById('filters-container');
-        const allButton = document.createElement('button');
-        allButton.textContent = "Tous";
-        allButton.classList.add("category");
-        allButton.addEventListener('click', () => filterWorks("all"));
-        filtersContainer.appendChild(allButton);
-        
-        categories.forEach(category => {
-            const button = document.createElement('button');
-            button.textContent = category.name;
-            button.dataset.id = category.id;
-            button.classList.add("category");
-            button.addEventListener('click', () => filterWorks(category.id));
-            filtersContainer.appendChild(button);
-        });
-        
-        console.log('Catégories récupérées');
-    } catch (error) {
-        console.error('Erreur lors de la récupération des catégories', error);
-    }
+    return filteredWorks;
 }
+init();
+
 
 
 
